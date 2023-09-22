7f5d21fd5098c47394bf5ed21ae8f4816ef8f6eb
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:07:16 +0500

    Exercise 3

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let myName = "Zawahir";
+let uppercasename = myName.toUpperCase();
+console.log(`${uppercasename}`);
+let lowercasename = myName.toLowerCase();
+console.log(`${lowercasename}`);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+
+let myName:string="Zawahir"
+let upperCaseName:string=myName.toUpperCase();
+console.log(`${upperCaseName}`)
+let lowerCaseName:string=myName.toLowerCase();
+console.log(`${lowerCaseName}`)
